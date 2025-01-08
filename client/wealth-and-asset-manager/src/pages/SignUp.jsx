126995f6378b3012de63f375453188a86d2474e1
Izien <izieniremiren@gmail.com>
2025-01-07 19:37:04 -0500

    adjusting sign up page

--- client/wealth-and-asset-manager/src/pages/SignUp.jsx
+++ client/wealth-and-asset-manager/src/pages/SignUp.jsx
@@ -54,7 +54,7 @@ const SignUp = ({ setToken }) => {
   };
 
   return (
-    <div className="flex items-center justify-center bg-gray-100">
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <form className="max-w-md w-full bg-white p-6 rounded-lg shadow-md">
         <div className="grid md:grid-cols-2 md:gap-6">
           <div className="relative z-0 w-full mb-5 group">
